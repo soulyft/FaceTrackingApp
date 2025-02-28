161f08899696eb65b66f40681c0a751ee74ed03f
Corey Lofthus <corey@soulyftaudio.com>
2025-02-28 11:50:15 -0800

    Initial Commit

---
+++ FaceTrackingApp/FaceTrackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceTrackingAppApp.swift
+//  FaceTrackingApp
+//
+//  Created by Corey Lofthus on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FaceTrackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
